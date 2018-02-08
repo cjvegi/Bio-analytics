2d0e8db6a2c089a103d099b1ae47ee48fbeb8184
Chiranjeevi Vegi <vegi@umich.edu>
2018-02-08 15:57:27 -0500

    TensorBoard with Images

---
+++ TensorBoard/TensorBoard with Sprite Images/code/Generate_sprite_images.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 16:25:24 2018
+
+@author: Khan
+"""
+
+# -*- coding: utf-8 -*-
+
+
+import os,cv2
+import numpy as np
+
+PATH = os.getcwd()+'/brain'
+
+LOG_DIR = PATH+ '/embedding-logs'
+
+
+data_path = PATH + '/bio'
+data_dir_list = os.listdir(data_path)
+
+img_data=[]
+for img in data_dir_list:
+    input_img=cv2.imread(data_path +'/'+ img )
+    input_img_resize=cv2.resize(input_img,(64,64))
+    img_data.append(input_img_resize)
+    
+                
+img_data = np.array(img_data)
+    
+def images_to_sprite(data):
+    if len(data.shape) == 3:
+        data = np.tile(data[...,np.newaxis], (1,1,1,3))
+    data = data.astype(np.float32)
+    min = np.min(data.reshape((data.shape[0], -1)), axis=1)
+    data = (data.transpose(1,2,3,0) - min).transpose(3,0,1,2)
+    max = np.max(data.reshape((data.shape[0], -1)), axis=1)
+    data = (data.transpose(1,2,3,0) / max).transpose(3,0,1,2)
+    # Inverting the colors seems to look better for MNIST
+    #data = 1 - data
+
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = ((0, n ** 2 - data.shape[0]), (0, 0),
+            (0, 0)) + ((0, 0),) * (data.ndim - 3)
+    data = np.pad(data, padding, mode='constant',
+            constant_values=0)
+    # Tile the individual thumbnails into an image.
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3)
+            + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    data = (data * 255).astype(np.uint8)
+    return data
+
+sprite = images_to_sprite(img_data)
+cv2.imwrite(os.path.join(LOG_DIR, 'sprite_4_classes.png'), sprite)
+
---
+++ TensorBoard/TensorBoard with Sprite Images/code/tensor_board.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 12:13:17 2018
+
+@author: Khan
+"""
+
+# -*- coding: utf-8 -*-
+
+import os
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.tensorboard.plugins import projector
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+
+PATH = os.getcwd()
+
+LOG_DIR = PATH + '/mnist-tensorboard/log-1/'
+metadata = os.path.join(LOG_DIR, 'df_labels.tsv')
+
+df = pd.read_csv("scaled.csv",index_col =0)
+df = df.drop(['cluster_2','cluster_3'], axis = 1)
+pca = PCA(n_components=300,
+         random_state = 123,
+         svd_solver = 'auto'
+         )
+
+df_pca = pd.DataFrame(pca.fit_transform(df))
+
+mnist = df_pca.values 
+
+
+#Feature Scaling
+'''
+sc_X = StandardScaler()
+mnist = sc_X.fit_transform(mnist)
+'''
+#mnist = np.asarray((mnist))
+images = tf.Variable(mnist)
+#def save_metadata(file):
+'''
+with open(metadata, 'w') as metadata_file:
+    for row in range(10000):
+        c = np.nonzero(mnist.test.labels[::1])[1:][0][row]
+        metadata_file.write('{}\n'.format(c))
+'''
+
+
+with tf.Session() as sess:
+    saver = tf.train.Saver([images])
+
+    sess.run(images.initializer)
+    saver.save(sess, os.path.join(LOG_DIR, 'images.ckpt'))
+
+    config = projector.ProjectorConfig()
+    # One can add multiple embeddings.
+    embedding = config.embeddings.add()
+    embedding.tensor_name = images.name
+    # Link this tensor to its metadata file (e.g. labels).
+    embedding.metadata_path = metadata
+    # Saves a config file that TensorBoard will read during startup.
+    embedding.sprite.image_path = os.path.join(LOG_DIR, 'sprite_32classes.png')
+    embedding.sprite.single_image_dim.extend([32, 32]) 
+    projector.visualize_embeddings(tf.summary.FileWriter(LOG_DIR), config) 
+
+
+#  tensorboard --logdir=C:\Users\Khan\Desktop\Files\mnist-tensorboard/log-1 --port=6006
